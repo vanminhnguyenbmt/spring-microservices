682745ccb671ffffc586835f565f84159752af88
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2020-12-30 15:36:33 +0700

    Configuring authentication service

---
+++ auth-service/src/main/java/com/example/auth/SecurityConfig/UserCredentials.java
@@ -0,0 +1,22 @@
+package com.example.auth.SecurityConfig;
+
+// A (temporary) class just to represent the user credentials
+public class UserCredentials {
+    private String username, password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
